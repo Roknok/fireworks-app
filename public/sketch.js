a9379b1ed251f6d37a230e312143f7daebe7e44f
Roknok <roychowdhuryraunak0110@gmail.com>
2025-04-28 16:23:28 +0530

    Describe your changes here

--- public/sketch.js
+++ public/sketch.js
@@ -1,18 +1,34 @@
 let fireworks = [];
+let lastFireworkTime = 0;  // Time when the last firework was triggered
+let cooldown = 2000;  // Cooldown period in milliseconds (2 seconds)
+let remainingCooldown = 0;  // Remaining time for cooldown
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
   background(0);
   socket = io.connect("https://fireworks-app.onrender.com/");  
   socket.on("firework", (data) => {
-    fireworks.push(new Firework(data.x*windowWidth, data.y*windowHeight)); 
+    fireworks.push(new Firework(data.x * windowWidth, data.y * windowHeight)); 
   });
+  lastFireworkTime = -2000
 }
 
 function draw() {
   background(0, 0.1); // fade trails slowly
 
+  // Update the cooldown timer
+  let currentTime = millis();
+  if (currentTime - lastFireworkTime < cooldown) {
+    remainingCooldown = cooldown - (currentTime - lastFireworkTime);
+  } else {
+    remainingCooldown = 0;
+  }
+
+  // Show the cooldown timer
+  displayCooldown(remainingCooldown);
+
+  // Update and show fireworks
   for (let i = fireworks.length - 1; i >= 0; i--) {
     fireworks[i].update();
     fireworks[i].show();
@@ -23,8 +39,20 @@ function draw() {
 }
 
 function mousePressed() {
-  socket.emit("firework", {x: mouseX/windowWidth,y: mouseY/windowHeight});
-  fireworks.push(new Firework(mouseX, mouseY));
+  if (remainingCooldown <= 0) {
+    socket.emit("firework", { x: mouseX / windowWidth, y: mouseY / windowHeight });
+    fireworks.push(new Firework(mouseX, mouseY));
+    lastFireworkTime = millis();  // Reset the timer
+  }
+}
+
+function displayCooldown(timeLeft) {
+  // Display the remaining cooldown in the center-bottom of the screen
+  textAlign(CENTER, CENTER);
+  textSize(64);
+  noStroke()
+  fill(20, 0.3);  // Light gray with small opacity
+  text(Math.ceil(timeLeft / 1000), width / 2, height - 50);  // Display seconds
 }
 
 class Firework {
